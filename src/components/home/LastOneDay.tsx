a93caa6159e2f3065d5b99c29a9fb11fc28af19d
jinhyukSeo777 <77399457+jinhyukSeo777@users.noreply.github.com>
2024-09-26 23:47:05 +0900

    Feat: add home page
    홈 화면 제작

---
+++ src/components/home/LastOneDay.tsx
@@ -0,0 +1,160 @@
+import styled from "styled-components";
+import { g3, SECONDARY_COLOR } from "../../utils/color";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { IData } from "../../pages/Home";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import Item from "../common/Item";
+import { PHONE } from "../../utils/size";
+
+const Container = styled.section`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin: 5rem 0;
+`;
+
+const H2 = styled.h2`
+  position: absolute;
+  top: -6rem;
+  left: 0;
+  font-size: 2rem;
+`;
+
+const Wrap = styled.div`
+  width: 100%;
+  position: relative;
+  @media (max-width: 1600px) {
+    width: 1250px;
+  }
+  @media (max-width: 1440px) {
+    width: 950px;
+  }
+  @media (max-width: 1100px) {
+    width: 700px;
+  }
+  @media (max-width: 800px) {
+    width: 450px;
+  }
+  @media (max-width: 600px) {
+    width: 200px;
+  }
+  @media (max-width: ${PHONE}) {
+    margin-top: 3rem;
+  }
+`;
+
+const CustomNavButton = styled.div`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  color: ${g3};
+  cursor: pointer;
+  z-index: 9;
+  font-size: 2.5rem;
+  &:hover {
+    color: ${SECONDARY_COLOR};
+  }
+`;
+
+const PrevButton = styled(CustomNavButton)`
+  left: -3rem;
+`;
+
+const NextButton = styled(CustomNavButton)`
+  right: -3rem;
+`;
+
+const LastOneDay = () => {
+  const formatDateToYYYYMMDD = (date: Date) => {
+    const year = date.getFullYear();
+    let month = (date.getMonth() + 1).toString().padStart(2, "0"); // 월은 0부터 시작하므로 1을 더함
+    let day = date.getDate().toString().padStart(2, "0"); // 날짜도 2자리로 맞춤
+
+    return `${year}${month}${day}`;
+  };
+
+  const getTomorrow = () => {
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
+    return tomorrow;
+  };
+
+  const fetchData = async (): Promise<IData[]> => {
+    const KEY = process.env.REACT_APP_KEY;
+    const tomorrow = getTomorrow();
+    const formattedDate = formatDateToYYYYMMDD(tomorrow);
+
+    const { data } = await axios.get(
+      `https://openapi.gg.go.kr/AbdmAnimalProtect?KEY=${KEY}&PBLANC_END_DE=${formattedDate}&Type=json&pSize=20`
+    );
+    return data.AbdmAnimalProtect[1].row;
+  };
+
+  const { data } = useQuery({
+    queryKey: ["LastOneDay"],
+    queryFn: fetchData,
+  });
+
+  return (
+    <Container>
+      <H2>공고기간이 하루 남은 친구들이에요!</H2>
+      <Wrap>
+        <PrevButton className="custom-prev">
+          <FontAwesomeIcon icon={faAngleLeft}></FontAwesomeIcon>
+        </PrevButton>
+        <NextButton className="custom-next">
+          <FontAwesomeIcon icon={faAngleRight}></FontAwesomeIcon>
+        </NextButton>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={40}
+          loop={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          navigation={{
+            nextEl: ".custom-next", // 커스텀 네비게이션 버튼과 연결
+            prevEl: ".custom-prev",
+          }}
+          modules={[Navigation, Autoplay]}
+          className="mySwiper"
+          breakpoints={{
+            600: {
+              slidesPerView: 2,
+            },
+            800: {
+              slidesPerView: 3,
+            },
+            1100: {
+              slidesPerView: 4,
+            },
+            1440: {
+              slidesPerView: 5,
+            },
+            1600: {
+              slidesPerView: 6,
+            },
+          }}
+        >
+          {data?.map((value, index) => (
+            <SwiperSlide key={index}>
+              <Item data={value} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Wrap>
+    </Container>
+  );
+};
+
+export default LastOneDay;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,154 @@
+import styled from "styled-components";
+import { DESKTOP, PHONE, TABLET } from "../utils/size";
+import { ReactComponent as MyIcon } from "../assets/icons/paw.svg";
+import { ReactComponent as MyBg } from "../assets/images/bg.svg";
+import { MAIN_COLOR } from "../utils/color";
+import LastOneDay from "../components/home/LastOneDay";
+import Charts from "../components/home/Charts";
+
+const Container = styled.main`
+  width: 90%;
+  max-width: ${DESKTOP};
+  margin: 0 auto;
+  margin-top: 5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  @media (max-width: ${PHONE}) {
+    align-items: center;
+  }
+`;
+
+const Banner = styled.section`
+  width: 100%;
+  height: 50rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+`;
+
+const BannerArea = styled.div`
+  z-index: 9;
+  div {
+    width: 80%;
+    span {
+      font-size: 3rem;
+      font-weight: bold;
+      line-height: 4rem;
+      &:nth-child(2) {
+        color: ${MAIN_COLOR};
+      }
+    }
+  }
+  p {
+    width: 80%;
+    margin: 2rem 0;
+    font-size: 1.3rem;
+    line-height: 2rem;
+  }
+  button {
+    background-color: ${MAIN_COLOR};
+    border: none;
+    color: white;
+    padding: 0.8rem;
+    border-radius: 1rem;
+    font-size: 1.1rem;
+    font-weight: 600;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
+  @media (max-width: ${TABLET}) {
+    div {
+      width: 100%;
+      span {
+        font-size: 2.5rem;
+        line-height: 3.7rem;
+      }
+    }
+    p {
+      margin: 2rem 0;
+      font-size: 1.2rem;
+      line-height: 2rem;
+    }
+  }
+  @media (max-width: ${PHONE}) {
+    div {
+      width: 100%;
+      span {
+        font-size: 2rem;
+        line-height: 3rem;
+      }
+    }
+    p {
+      margin: 2rem 0;
+      font-size: 1rem;
+      line-height: 1.6rem;
+    }
+  }
+`;
+
+const StyledBg = styled(MyBg)`
+  height: 640px;
+  @media (max-width: ${TABLET}) {
+    position: absolute;
+    width: 100%;
+    height: auto;
+    opacity: 0.4;
+  }
+`;
+
+export interface IData {
+  SPECIES_NM: string; //품종
+  SEX_NM: "M" | "F"; //성별
+  NEUT_YN: string; //중성화여부
+  AGE_INFO: string; //나이
+  BDWGH_INFO: string; //체중
+  RECEPT_DE: string; //접수일시
+  DISCVRY_PLC_INFO: string; //발견장소
+  SFETR_INFO: string; //특징
+  PBLANC_BEGIN_DE: string; //공고시작
+  PBLANC_END_DE: string; //공고마감
+  SHTER_NM: string; //보호센터
+  REFINE_ROADNM_ADDR: string; //센터주소
+  SHTER_TELNO: string; //연락처
+  IMAGE_COURS: string; //이미지
+  THUMB_IMAGE_COURS: string; //썸네일
+  REFINE_WGS84_LAT: string; //위도
+  REFINE_WGS84_LOGT: string; //경도
+  SIGUN_NM: string; //시군명
+  STATE_NM: string; //상태
+  ABDM_IDNTFY_NO: string; //고유번호
+}
+
+const Home = () => {
+  return (
+    <Container>
+      <Banner>
+        <BannerArea>
+          <div>
+            <span>버려진 아이들과 당신의</span>
+            <span>운명적 만남</span>
+            <span>, 찾고 계신가요?</span>
+          </div>
+          <p>
+            지금 당신의 따뜻한 마음을 기다리는 친구들이 있습니다. 유기동물
+            입양으로 가족이 되어주세요.
+          </p>
+          <button>
+            나의 반려동물 찾기
+            <MyIcon style={{ marginLeft: "0.3rem" }} width={30} height={30} />
+          </button>
+        </BannerArea>
+        <StyledBg />
+      </Banner>
+      <LastOneDay />
+      <Charts />
+    </Container>
+  );
+};
+
+export default Home;
