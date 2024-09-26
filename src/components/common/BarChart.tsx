072abc6d0dbfb359ddb6704233a2c31c16ad2870
jinhyukSeo777 <77399457+jinhyukSeo777@users.noreply.github.com>
2024-09-26 23:47:37 +0900

    Feat: add chart to home
    홈에 차트 추가

---
+++ src/components/common/BarChart.tsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+} from "chart.js";
+import { TABLET } from "../../utils/size";
+
+const Container = styled.div`
+  width: 50%;
+  @media (max-width: ${TABLET}) {
+    width: 100%;
+    margin: 5rem 0;
+  }
+`;
+
+interface IProps {
+  data: {
+    labels: string[];
+    datasets: {
+      label: string;
+      data: number[];
+      backgroundColor: string[];
+    }[];
+  };
+}
+
+const BarChart = ({ data }: IProps) => {
+  ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip);
+
+  return (
+    <Container>
+      <Bar
+        data={data}
+        options={{
+          indexAxis: "y",
+          plugins: {
+            tooltip: {
+              enabled: true,
+              callbacks: {
+                label: function (tooltipItem) {
+                  const label = tooltipItem.label || "";
+                  const value = tooltipItem.raw || 0;
+                  return `${label}: ${value} 마리`;
+                },
+              },
+            },
+            legend: {
+              display: false, // 범례 표시
+            },
+          },
+        }}
+      />
+    </Container>
+  );
+};
+
+export default BarChart;
---
+++ src/components/common/DonutChart.tsx
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+import { Doughnut } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+  ArcElement,
+} from "chart.js";
+import { TABLET } from "../../utils/size";
+
+const Container = styled.div`
+  width: 40%;
+  @media (max-width: ${TABLET}) {
+    width: 100%;
+  }
+`;
+
+interface IProps {
+  data: {
+    labels: string[];
+    datasets: {
+      label: string;
+      data: number[];
+      backgroundColor: string[];
+    }[];
+  };
+}
+
+const DonutChart = ({ data }: IProps) => {
+  ChartJS.register(CategoryScale, LinearScale, ArcElement, Tooltip, Legend);
+
+  return (
+    <Container>
+      <Doughnut
+        data={data}
+        options={{
+          plugins: {
+            tooltip: {
+              enabled: true,
+              callbacks: {
+                label: function (tooltipItem) {
+                  const label = tooltipItem.label || "";
+                  const value = tooltipItem.raw || 0;
+                  return `${label}: ${value} 마리`;
+                },
+              },
+            },
+            legend: {
+              labels: {
+                padding: 10,
+              },
+            },
+          },
+        }}
+      />
+    </Container>
+  );
+};
+
+export default DonutChart;
