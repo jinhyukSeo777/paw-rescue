8112fcb3b8a891464aa19ba1fe79f4a8333afdca
jinhyukSeo777 <77399457+jinhyukSeo777@users.noreply.github.com>
2024-09-26 23:49:13 +0900

    Feat: create animal item
    동물 정보 박스 제작

---
+++ src/components/common/Item.tsx
@@ -0,0 +1,134 @@
+import styled from "styled-components";
+import { IData } from "../../pages/Home";
+import { SECONDARY_COLOR } from "../../utils/color";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark as FullBookmark } from "@fortawesome/free-solid-svg-icons";
+import { faBookmark as ImptyBookmark } from "@fortawesome/free-regular-svg-icons";
+import { useEffect, useState } from "react";
+
+const Container = styled.div`
+  position: relative;
+  width: 200px;
+  cursor: pointer;
+`;
+
+const Photo = styled.div<{ $url: string }>`
+  width: 200px;
+  height: 200px;
+  border-radius: 0.5rem;
+  background: url(${(props) => props.$url}) no-repeat center;
+  background-size: cover;
+`;
+
+const Info = styled.div`
+  margin-top: 1rem;
+  div {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 0.3rem;
+    &:first-child {
+      span {
+        font-weight: bold;
+        margin-bottom: 0.5rem;
+      }
+    }
+    &:not(:first-child) {
+      span {
+        &:first-child {
+          opacity: 0.6;
+        }
+      }
+    }
+  }
+`;
+
+const State = styled.div`
+  position: absolute;
+  top: 0.5rem;
+  left: 0.5rem;
+  padding: 0.5rem;
+  background-color: ${SECONDARY_COLOR};
+  color: white;
+  border-radius: 1rem;
+  font-size: 0.8rem;
+`;
+
+interface IProps {
+  data: IData;
+}
+
+const Item = ({ data }: IProps) => {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const getLikeList = () => {
+    const obj = localStorage.getItem("like");
+    const likeList: IData[] = obj ? JSON.parse(obj) : null;
+    return likeList || [];
+  };
+
+  useEffect(() => {
+    const likeList = getLikeList();
+    const isIn = likeList.some(
+      (value) => value.ABDM_IDNTFY_NO === data.ABDM_IDNTFY_NO
+    );
+    setIsLiked(isIn);
+  }, [data.ABDM_IDNTFY_NO]);
+
+  const handleLike = () => {
+    const likeList = getLikeList();
+    if (isLiked) {
+      const newList = likeList.filter(
+        (value) => value.ABDM_IDNTFY_NO !== data.ABDM_IDNTFY_NO
+      );
+      localStorage.setItem("like", JSON.stringify(newList));
+      setIsLiked(false);
+    } else {
+      likeList.push(data);
+      localStorage.setItem("like", JSON.stringify(likeList));
+      setIsLiked(true);
+    }
+  };
+
+  return (
+    <Container>
+      <Photo $url={data.IMAGE_COURS}></Photo>
+      <Info>
+        <div>
+          <span>{data.SPECIES_NM}</span>
+          <span onClick={handleLike}>
+            {isLiked ? (
+              <FontAwesomeIcon
+                style={{ color: SECONDARY_COLOR }}
+                icon={FullBookmark}
+              />
+            ) : (
+              <FontAwesomeIcon
+                style={{ color: SECONDARY_COLOR }}
+                icon={ImptyBookmark}
+              />
+            )}
+          </span>
+        </div>
+        <div>
+          <span>나이</span>
+          <span>{data.AGE_INFO}</span>
+        </div>
+        <div>
+          <span>시도군</span>
+          <span>{data.SIGUN_NM}</span>
+        </div>
+        <div>
+          <span>성별</span>
+          <span>{data.SEX_NM === "M" ? "남아" : "여아"}</span>
+        </div>
+        <div>
+          <span>중성화</span>
+          <span>{data.NEUT_YN === "Y" ? "중성화 완료" : "중성화 미완료"}</span>
+        </div>
+      </Info>
+      <State>{data.STATE_NM}</State>
+    </Container>
+  );
+};
+
+export default Item;
