f3a7b9403686a0e8733983f2f7025ec7f0c90d1e
jinhyukSeo777 <77399457+jinhyukSeo777@users.noreply.github.com>
2024-09-27 17:00:59 +0900

    Feat: add useCallback for optimization
    useCallback 추가

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,12 +1,12 @@
 import styled from "styled-components";
-import { DESKTOP, PHONE } from "../utils/size";
+import { DESKTOP } from "../utils/size";
 import LastOneDay from "../components/home/LastOneDay";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { IData } from "./Home";
 import Dropdown from "../components/common/Dropdown";
 import { LEGION as LEGIONS } from "../components/home/Charts";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import ShowItems from "../components/common/ShowItems";
 import Pagenation from "../components/common/Pagenation";
 
@@ -66,53 +66,71 @@ const List = () => {
     setFilteredData(data);
   }, [data]);
 
-  const filterLegion = (value: string) => {
-    if (legion === "전체") return true;
-    if (value === legion) return true;
-    return false;
-  };
-  const filterState = (value: string) => {
-    if (state === "전체") return true;
-    if (value.includes(state)) return true;
-    return false;
-  };
-  const filterSpecies = (value: string) => {
-    if (species === "전체") return true;
-    if (species === "강아지" && value.includes("개")) return true;
-    if (species === "고양이" && value.includes("고양이")) return true;
-    if (
-      species === "그외" &&
-      !value.includes("개") &&
-      !value.includes("고양이")
-    )
-      return true;
-    return false;
-  };
-  const filterAge = (value: string) => {
-    const today = new Date();
-    const year = today.getFullYear();
-    const birthYear = +value.slice(0, 4);
-    const diff = year - birthYear;
-    if (age === "전체") return true;
-    if (age === "1세미만" && diff === 0) return true;
-    if (age === "1살~5살" && diff >= 1 && diff <= 4) return true;
-    if (age === "6살~9살" && diff >= 5 && diff <= 8) return true;
-    if (age === "10살이상" && diff >= 9) return true;
-    return false;
-  };
-  const filterSex = (value: string) => {
-    if (sex === "전체") return true;
-    if (sex === "남아" && value === "M") return true;
-    if (sex === "여아" && value === "F") return true;
-    return false;
-  };
-  const filterNeut = (value: string) => {
-    if (neut === "전체") return true;
-    if (neut === "완료" && value === "Y") return true;
-    if (neut === "미완료" && value === "N") return true;
-    if (neut === "알수없음" && value === "U") return true;
-    return false;
-  };
+  const filterLegion = useCallback(
+    (value: string) => {
+      if (legion === "전체") return true;
+      if (value === legion) return true;
+      return false;
+    },
+    [legion]
+  );
+  const filterState = useCallback(
+    (value: string) => {
+      if (state === "전체") return true;
+      if (value.includes(state)) return true;
+      return false;
+    },
+    [state]
+  );
+  const filterSpecies = useCallback(
+    (value: string) => {
+      if (species === "전체") return true;
+      if (species === "강아지" && value.includes("개")) return true;
+      if (species === "고양이" && value.includes("고양이")) return true;
+      if (
+        species === "그외" &&
+        !value.includes("개") &&
+        !value.includes("고양이")
+      )
+        return true;
+      return false;
+    },
+    [species]
+  );
+  const filterAge = useCallback(
+    (value: string) => {
+      const today = new Date();
+      const year = today.getFullYear();
+      const birthYear = +value.slice(0, 4);
+      const diff = year - birthYear;
+      if (age === "전체") return true;
+      if (age === "1세미만" && diff === 0) return true;
+      if (age === "1살~5살" && diff >= 1 && diff <= 4) return true;
+      if (age === "6살~9살" && diff >= 5 && diff <= 8) return true;
+      if (age === "10살이상" && diff >= 9) return true;
+      return false;
+    },
+    [age]
+  );
+  const filterSex = useCallback(
+    (value: string) => {
+      if (sex === "전체") return true;
+      if (sex === "남아" && value === "M") return true;
+      if (sex === "여아" && value === "F") return true;
+      return false;
+    },
+    [sex]
+  );
+  const filterNeut = useCallback(
+    (value: string) => {
+      if (neut === "전체") return true;
+      if (neut === "완료" && value === "Y") return true;
+      if (neut === "미완료" && value === "N") return true;
+      if (neut === "알수없음" && value === "U") return true;
+      return false;
+    },
+    [neut]
+  );
 
   useEffect(() => {
     const newData = data?.filter((value) => {
